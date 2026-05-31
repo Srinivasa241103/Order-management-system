241e8df753c603f87b944e46c639b24efada0d4b
Cherry <srinivasashankar8@gmail.com>
2026-05-31 17:49:46 +0530

    containarised backend with postgres

--- server/app/repositories/__init__.py
+++ server/app/repositories/__init__.py
@@ -1,5 +1,5 @@
 from app.repositories.customer_repository import CustomerRepository
-from app.repositories.order_repositroy import OrderRepository
+from app.repositories.order_repository import OrderRepository
 from app.repositories.product_repository import ProductRepository
 
 __all__ = [
